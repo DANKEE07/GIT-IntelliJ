dcd9da2af030480d6be8ae3636f9a42273612753
sergi <sergiogtrrzcbzs@gmail.com>
2026-03-12 17:24:05 +0100

    zx

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
         System.out.println("Hola Mundo!");
         System.out.println("La fecha actual es: "+java.time.LocalDate.now());
         System.out.println("La hora actual es: "+java.time.LocalTime.now());
-        /*sergio*/
+        System.out.println("La fecha y hora actual son: "+java.time.LocalDateTime.now());
     }
 }
