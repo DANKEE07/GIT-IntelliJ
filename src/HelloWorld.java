5a373652052dc71ecf3b1ae5161908375fd53ff1
Rafael <Am6brafael@gmail.com>
2026-03-18 20:33:51 +0100

    añadido nuevo a helloWord

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld {
         System.out.println("Small dih big dreams🥀");
         System.out.println("La hora actual es: "+java.time.LocalTime.now());
         System.out.println("La fecha y hora actual son: "+java.time.LocalDateTime.now());
+
+
+        System.out.println("Buenas noches");
     }
 }
