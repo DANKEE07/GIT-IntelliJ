9ad0c90609acb226d996c97654812f1c0b5bb4b7
sergi <sergiogtrrzcbzs@gmail.com>
2026-03-18 23:03:00 +0100

    He añadido un nuevo print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Small dih big dreams🥀");
         System.out.println("La hora actual es: "+java.time.LocalTime.now());
         System.out.println("La fecha y hora actual son: "+java.time.LocalDateTime.now());
+        System.out.println("al lope le gustan los femboys");
     }
 }
